49297ac65d8c61a256bb8e6bdaa46cdf417630e1
Nilesh Suthar <nileshsuthar@live.in>
2013-10-21 01:59:22 +0530

    Random number for bingo are now generated

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,23 @@ exports.index = function(req, res){
   res.render('index', { title: 'Bingo',page:'index' });
 };
 exports.bingo = function(req,res){
-    res.render('bingo',{page:'bingo'});
+    var number = getBingoCard(1,100,25);
+    res.render('bingo',{page:'bingo',number:number});
+}
+function getBingoCard(min,max,no){
+    var ob={};
+    var number = [];
+    for(var i=0;i<no;i++){
+        var temp = Math.floor((Math.random()*max)+min);
+        if(ob[temp]==null){
+            number.push(temp)
+        }
+        ob[temp]=temp;
+        if((i+1)==no && Object.keys(ob).length!=no){
+            i=i-(no-Object.keys(ob).length);
+            //console.log('in obj i modifier');
+        }
+    }
+    //console.log(ob,Object.keys(ob).length,number.length);
+    return number;
 }
