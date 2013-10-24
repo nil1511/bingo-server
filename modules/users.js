a0dbac012c66b0796ebe31f591042f5a51b306cb
Nilesh Suthar <nileshsuthar@live.in>
2013-10-24 19:44:12 +0530

    Cleared module user

--- modules/users.js
+++ modules/users.js
@@ -1,19 +1,3 @@
-function User(socket,number,id){
-    if(socket)
-    this.socket=socket;
-    if(number)
-    this.number=number;
-    this.id=id;
-}
-
-User.prototype.setSocket = function(socket){
-    this.socket=socket;
-}
-User.prototype.setNum = function(number){
-    this.number=number;
-}
-
-//exports.users=this.user;
 var user={};
 exports.AddUser = function(id){
     console.log("Inside add user module user");
@@ -23,13 +7,10 @@ exports.AddUser = function(id){
 exports.setSocket = function(id,socket){
    console.log("Inside setSocket module user");
    user[id]['socket']=socket;
-   //console.log(user[id]);
-   //console.log("Uses"+user+"dewf");
 }
 exports.setNum = function(id,num){
    console.log("Inside setNum module user");
    user[id]['num']=num;
-   //console.log("Uses"+user+"dewf");
 
 }
 exports.getNum = function(id){
